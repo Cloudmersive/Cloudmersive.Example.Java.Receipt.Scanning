5c7648809cd71f11ef6378138f7cc917357b7aa2
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-12-18 07:24:13 -0800

    Update App.java

--- example/src/main/java/main/App.java
+++ example/src/main/java/main/App.java
@@ -1,5 +1,14 @@
 package pkg;
 
+import com.cloudmersive.client.invoker.ApiClient;
+import com.cloudmersive.client.invoker.ApiException;
+import com.cloudmersive.client.invoker.Configuration;
+import com.cloudmersive.client.invoker.auth.*;
+
+import java.io.File;
+
+import com.cloudmersive.client.GenerateBarcodeApi;
+
 /**
  * Hello world!
  */
@@ -12,6 +21,22 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        // Configure API key authorization: Apikey
+        ApiKeyAuth Apikey = (ApiKeyAuth) defaultClient.getAuthentication("Apikey");
+        Apikey.setApiKey("YOUR API KEY");
+        // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+        //Apikey.setApiKeyPrefix("Token");
+
+        GenerateBarcodeApi apiInstance = new GenerateBarcodeApi();
+        String value = "value_example"; // String | QR code text to convert into the QR code barcode
+        try {
+            File result = apiInstance.generateBarcodeQRCode(value);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling GenerateBarcodeApi#generateBarcodeQRCode");
+            e.printStackTrace();
+        }
     }
 }
